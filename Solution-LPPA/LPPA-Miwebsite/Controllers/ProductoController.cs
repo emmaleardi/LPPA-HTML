1814ded62ac8e92a10525f22caf6665f0d705a7f
Emmanuel Leardi <emmaleardi@hotmail.com>
2020-04-21 00:19:34 -0300

    Toma de datos formulario

--- Solution-LPPA/LPPA-Miwebsite/Controllers/ProductoController.cs
+++ Solution-LPPA/LPPA-Miwebsite/Controllers/ProductoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LPPA_Miwebsite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,32 @@ using System.Web.Mvc;
 
 namespace LPPA_Miwebsite.Controllers
 {
+    
     public class ProductoController : Controller
-    {
+    { 
+        
         // GET: Producto
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            //
+            var producto = new Producto();
+            producto.Codigo = Convert.ToInt32(form["codigo"]);
+            producto.Nombre = form["nombre"];
+            producto.Descripcion = form["descripcion"];
+
+            Dominio.Add(producto);
+
+            return RedirectToAction("Index");
         }
     }
 }
